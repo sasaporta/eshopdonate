9574339358e1cb9d3f1fbf8a8b873877ab9daad5
Steve Saporta <sasaporta@gmail.com>
2013-01-11 00:32:44 +0000

    Add some static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Buydonate::Application.routes.draw do
+  get "static_pages/about"
+
+  get "static_pages/privacy"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

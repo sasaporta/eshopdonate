91c4d07c5f7ae4e80d7bfbeedbd91769eafa7f1e
Steve Saporta <sasaporta@gmail.com>
2013-01-18 21:13:38 +0000

    edit and update charity are working

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -14,6 +14,30 @@ class CharitiesController < ApplicationController
     @charity = Charity.find(params[:id])
   end
 
+  def new
+    @charity = Charity.new
+  end
+
+  def create()
+    @charity = Charity.new(params[:charity])
+    if @charity.save
+      flash[:success] = "New charity added"
+      redirect_to charities_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @charity = Charity.find(params[:id])
+    if @charity.update_attributes(params[:charity])
+      flash[:success] = "Charity updated"
+      redirect_to charities_path
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     Charity.find(params[:id]).delete
     redirect_to charities_path and return

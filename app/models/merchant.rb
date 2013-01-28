3078c7fc7951b8421656dccf29f7a26f4bd7167d
Steve Saporta <sasaporta@gmail.com>
2013-01-28 14:22:19 +0000

    Allow longer names

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,7 @@
 class Merchant < ActiveRecord::Base
   attr_accessible :name, :link, :img_url, :img_alt, :tracking_img, :percentage
 
-  validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: { maximum: 200 }, uniqueness: { case_sensitive: false }
   validates :link, presence: true, length: { maximum: 200 }
   validates :img_url, presence: true, length: { maximum: 200 }
   validates :img_alt, presence: true, length: { maximum: 200 }
